259667a2a5470fd72418e86eb21ebc0d1d52fae4
Tymoteusz Gach <tymoteusz.gach@merapar.com>
2021-09-29 18:54:42 +0200

    renamed parameter

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -9,7 +9,7 @@ export type Command<Type extends string = string,
 export function isCommand<Type extends string = string,
   Data extends Record<string, unknown> = Record<string, unknown>,
   // eslint-disable-next-line @typescript-eslint/no-explicit-any,@typescript-eslint/explicit-module-boundary-types
-  Metadata extends Record<string, unknown> = Record<string, unknown>>(event: any): event is Command<Type, Data, Metadata> {
-  return typeof event.type !== 'undefined'
-    && typeof event.data !== 'undefined'
+  Metadata extends Record<string, unknown> = Record<string, unknown>>(command: any): command is Command<Type, Data, Metadata> {
+  return typeof command.type !== 'undefined'
+    && typeof command.data !== 'undefined'
 }
